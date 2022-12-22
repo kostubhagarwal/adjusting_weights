2aaed40bb5fd984fd727c713ae9facda5baa7064
root <root@tubbypc.localdomain>
2022-12-22 01:37:38 -0500

    2am in pearson init

---
+++ main.py
@@ -0,0 +1,23 @@
+# new skill? old skill>
+
+# if old skill, then show list of old skills
+
+# show summary, select practice or review
+
+# if practice, then show list of practice items + add more
+
+# if review, then show all items
+
+#--------------------#
+
+# every skill is a directory
+
+# summary.txt
+
+# practice.txt
+
+# all.txt
+
+#--------------------#
+
+
